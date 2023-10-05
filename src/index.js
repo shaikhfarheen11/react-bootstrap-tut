7778cce54244100c1b6e49d1b03ef9c042b0169e
shaikhfarheen11 <farheens554>
2023-10-06 01:52:32 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import { AuthContextProvider } from './store/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <AuthContextProvider>
   <BrowserRouter>

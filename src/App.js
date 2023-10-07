df08e8903aa7f8c13ad2b4fdc2b717e280408b57
shaikhfarheen11 <farheens554>
2023-10-08 01:17:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Layout from './Component/Layout/Layout';
 import UserProfile from './Component/Profile/userProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/auth' element={<AuthPage />} />
-        <Route path='/profile' element={<UserProfile />} />
+        <Route path="/" element={<HomePage />} />
+        {!authCtx.isLoggedIn && (
+          <Route path="/auth" element={<AuthPage />} />
+        )}
+
+        <Route
+          path="/profile"
+          element={
+            authCtx.isLoggedIn ? (
+              <UserProfile />
+            ) : (
+              <Navigate to="/auth" replace />
+            )
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+     
     </Layout>
   );
 }

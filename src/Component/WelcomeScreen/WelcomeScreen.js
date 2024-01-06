3756ebc3e01a964665b392a08717e62e696f4f72
shaikhfarheen11 <farheens554>
2024-01-07 02:08:03 +0530

    first commit

--- src/Component/WelcomeScreen/WelcomeScreen.js
+++ src/Component/WelcomeScreen/WelcomeScreen.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import classes from './Welcome.module.css';
 
 
 
 
 const WelcomeScreen = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+
+    localStorage.removeItem('idToken');
+
+    navigate('/login');
+  };
 
   
   return (
   
+   
+
         <div className={classes.welcomeContainer}>
+          <div className={classes.logout} onClick={handleLogout}>
+      Logout
+    </div>
           <h2>Welcome to Expense Tracker!!!</h2>
           <div className={classes.incompleteYour}>
             <p>

01aec6f58ea6748384083a65cc5ba39ba75588db
shaikhfarheen11 <farheens554>
2024-01-06 23:01:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './Component/Signup/Signup';
-import Login from './Component/Login/Login';
-import WelcomeScreen from './Component/WelcomeScreen/WelcomeScreen';
-import WinningQuote from './Component/WelcomeScreen/WinningQoute';
 import { AuthContextProvider } from './Component/AuthContext/AuthContext';
-import ForgotPassword from './Component/ForgotPassword/ForgotPassword';
-import ExpenseForm from './Component/Expense/ExpenseForm';
-import ExpenseList from './Component/Expense/ExpenseList';
-import ExpenseEdit from './Component/Expense/ExpenseEdit';
-import LogoutConfirmation from './Component/LogoutPage/LogoutConfirmation';
 
 function App() {
   return (
@@ -19,15 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Signup />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/welcome" element={<WelcomeScreen />} />
-          <Route path="/winning-qoute" element={<WinningQuote />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/logout-confirmation" element={<LogoutConfirmation />} />
-          <Route path="/expense-form" element={<ExpenseForm />} />
-          <Route path="/welcome" element={<ExpenseList />} />
-          <Route path="/edit-expense/:id" element={<ExpenseEdit />} />
         </Routes>
       </BrowserRouter>
     </AuthContextProvider>

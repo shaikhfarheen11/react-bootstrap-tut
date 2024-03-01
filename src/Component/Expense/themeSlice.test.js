87438174327095466ad882f08485cba04d60dece
shaikhfarheen11 <farheens554>
2024-03-01 17:19:20 +0530

    first commit

--- src/Component/Expense/themeSlice.test.js
+++ src/Component/Expense/themeSlice.test.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import themeReducer, { toggleDarkMode, selectDarkMode } from './themeSlice';
+import themeReducer, { toggleDarkMode } from './themeSlice';
 
 describe('themeSlice', () => {
   let store;
